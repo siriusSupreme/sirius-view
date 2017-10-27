97490579b4275d38d47d8c3f765b06bc72790518
siriusSupreme <guoplc@163.com>
2017-10-27 09:23:20 +0800

    ''

---
+++ src/FileEngine.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Sirius\View\Engines;
+
+use Sirius\View\Contracts\Engine;
+
+class FileEngine implements Engine
+{
+    /**
+     * Get the evaluated contents of the view.
+     *
+     * @param  string  $path
+     * @param  array   $data
+     * @return string
+     */
+    public function get($path, array $data = [])
+    {
+        return file_get_contents($path);
+    }
+}
